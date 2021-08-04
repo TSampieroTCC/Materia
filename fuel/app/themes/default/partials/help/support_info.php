6fe8ec932935485f970fdc209a49002e48b62640
TSampieroTCC <88443382+TSampieroTCC@users.noreply.github.com>
2021-08-04 10:14:50 -0400

    Update support_info.php

--- fuel/app/themes/default/partials/help/support_info.php
+++ fuel/app/themes/default/partials/help/support_info.php
@@ -1,23 +1,10 @@
 <div class="error-support">
-	<h3>Trouble Logging In?</h3>
-	<span class="subtitle">Contact the Service Desk.</span>
-	<dl class="contact">
-		<dt>Website</dt>
-			<dd><a href="http://website/support">http://website/support</a></dd>
-		<dt>Email</dt>
-			<dd><a href="mailto:support@website">support@website</a></dd>
-		<dt>Phone</dt>
-			<dd>PHONE NUMBER HERE</dd>
-	</dl>
-
 	<h3>Get Help Using Materia</h3>
-	<span class="subtitle">When something's gone wrong, or you just need a hand.</span>
+	<span class="subtitle">Contact Travis Sampiero with TCC Online for help with using Materia.</span>
 	<dl class="online-support">
-		<dt>Materia Support</dt>
-			<dd><a href="http://website/support/">http://website/support/</a></dd>
 		<dt>Email</dt>
-			<dd><a href="mailto:support@website">support@website</a></dd>
+			<dd><a href="mailto:support@website">Travis.Sampiero@tcc.fl.edu</a></dd>
 		<dt>Phone</dt>
-			<dd>PHONE NUMBER HERE</dd>
+			<dd>(850)201-9417</dd>
 	</dl>
 </div>
